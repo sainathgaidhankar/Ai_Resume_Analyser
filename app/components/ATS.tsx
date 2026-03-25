2b6a854c1a7bb4ffbd535f926417de858e9b811f
sainath gaidhankar <sainathgaidhankar@gmail.com>
2026-03-25 10:02:52 +0530

    AI feedback generation

---
+++ app/components/ATS.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ATS = () => {
+  return (
+    <div>
+        ATS
+    </div>
+  )
+}
+
+export default ATS
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -62,6 +62,7 @@ const upload = () => {
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
         setStatusText('Analysis complete, rendering...');
         console.log(data);
+        navigate(`/resume/${uuid}`);
   }
 
   const handleSubmit = (e : FormEvent<HTMLFormElement>) => {
